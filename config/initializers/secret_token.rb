6df5ac759a2cfc6a82d679d3bbf067cd33157efc
mrevilpig <wadeliao1986@172-26-207-49.dynapool.nyu.edu>
2014-05-27 17:39:47 -0400

    Add navbar, body content and footer with demo version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FTSANY::Application.config.secret_key_base = 'baa9d93d7869cd33e7b9b707f7ff05c4b9b8dec2da496d0844ad1b36faebc2efbe89073d84a1748e1ff8da36a0770571347650d1cfb9df90f87a266cd5dd219c'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+FTSANY::Application.routes.draw do
+  root 'welcome#index'
+end
